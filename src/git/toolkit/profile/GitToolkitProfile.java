aacba7825ac38609c0944d9850fac1c519d64a51
NeoTRAN001 <neo_tran@outlook.com>
2022-01-15 19:56:42 -0600

    feat: create jpanels help and feature, desing UX/UI to Home

--- src/git/toolkit/profile/GitToolkitProfile.java
+++ src/git/toolkit/profile/GitToolkitProfile.java
@@ -4,6 +4,8 @@
  */
 package git.toolkit.profile;
 
+import git.toolkit.profile.UI.Home.Home;
+
 /**
  *
  * @author NeoTRAN
@@ -14,7 +16,7 @@ public class GitToolkitProfile {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Home().show();
     }
     
 }
